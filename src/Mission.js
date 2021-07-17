2cd27c8b129ce0fbee1ce0035daaf84d2dc1a6d2
MartinaM77 <martina.michlova77@gmail.com>
2021-07-17 23:14:19 +0200

    add Go back icon button to return to Dashboard from Mission

--- src/Mission.js
+++ src/Mission.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
+import { makeStyles } from '@material-ui/core/styles';
 import { useQuery, gql } from "@apollo/client";
 import { useHistory, useLocation } from 'react-router-dom';
 import DetailBox from './DetailBox.js';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    paddingBottom: '50px',
+  },
+  iconButton: {
+    padding: '0',
+  },
+}));
 
 const MISSION_QUERY = gql`
 query getLaunch($id: ID!){
@@ -34,6 +46,7 @@ query getLaunch($id: ID!){
 `;
 
 export default function Mission() {
+  const classes = useStyles();
   const history = useHistory();
   const location = useLocation();
   const search = location.search;
@@ -54,6 +67,17 @@ export default function Mission() {
 
   return (
     <Grid container spacing={0} justifyContent='center' className='mission'>
+      <Grid item xs={8} className={classes.root}>
+        <div title="Go back">
+          <IconButton
+            className={classes.iconButton}
+            aria-label='Go back'
+            onClick={() => history.goBack()}
+          >
+            <ArrowBackIcon fontSize='large' color='primary'/>
+          </IconButton>
+        </div>
+      </Grid>
       {loading || !launch ?
         <Grid item xs={8} className='loading-launches'>
           <CircularProgress />
