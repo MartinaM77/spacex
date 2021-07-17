4ca162c509566dd26f0d50e07bd995c22019082e
MartinaM77 <martina.michlova77@gmail.com>
2021-07-17 22:08:30 +0200

    add styled Mission detail box with data

---
+++ src/DetailBox.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import Moment from 'react-moment';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Button from '@material-ui/core/Button';
+import StarsImg from './stars.png';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    boxShadow: '0px 0px 2px 2px rgba(70, 77, 90, 0.7)',
+  },
+  media: {
+    height: 0,
+    paddingTop: '25%',
+    backgroundColor: '#12161e',
+  },
+  button: {
+    justifyContent: 'center',
+    paddingBottom: '32px',
+  }
+}));
+
+export default function DetailBox(props) {
+  const classes = useStyles();
+  const {
+    mission_name,
+    details,
+    launch_date_local,
+    launch_site,
+    links,
+    rocket,
+    ships
+  } = props;
+
+  const seeMoreLink =
+    links.article_link ? links.article_link
+    : links.video_link ? links.video_link
+    : null;
+
+  return (
+    <Card className={classes.root}>
+      <CardMedia
+        className={classes.media}
+        image={links.flickr_images[0] ? links.flickr_images[0] : StarsImg}
+        title={mission_name}
+      />
+      <CardHeader
+        title={mission_name}
+        subheader={
+          <div>
+            <Moment format='DD/MM/YYYY'>
+              {launch_date_local}
+            </Moment>
+            , {launch_site.site_name_long}
+          </div>
+        }
+      />
+      <CardContent>
+        <p>{details}</p>
+        <p>Rocket:
+          {rocket.rocket.wikipedia ?
+            <a href={rocket.rocket.wikipedia}> {rocket.rocket_name}</a>
+            : rocket.rocket_name
+          }
+        </p>
+        {ships.length > 0 &&
+          <p> Ships:
+            {ships.map((ship, index) => {
+              return (
+                <span key={ship+index}>{index > 0 ? ', ' : ' '}
+                {ship.url ?
+                  <a href={ship.url}> {ship.name}</a>
+                  : ship.name
+                }
+                </span>
+              )
+            })}
+          </p>
+        }
+      </CardContent>
+      {seeMoreLink &&
+        <CardActions className={classes.button}>
+          <a href={seeMoreLink} target="_blank" rel="noopener noreferrer">
+            <Button variant="contained">See more</Button>
+          </a>
+        </CardActions>
+      }
+    </Card>
+  );
+}
--- src/Mission.js
+++ src/Mission.js
@@ -1,9 +1,72 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import { useQuery, gql } from "@apollo/client";
+import { useHistory, useLocation } from 'react-router-dom';
+import DetailBox from './DetailBox.js';
+
+const MISSION_QUERY = gql`
+query getLaunch($id: ID!){
+  launch(id: $id) {
+    mission_name
+    details
+    launch_date_local
+    launch_site {
+      site_name_long
+    }
+    links {
+      article_link
+      flickr_images
+      video_link
+    }
+    rocket {
+      rocket_name
+      rocket {
+        wikipedia
+      }
+    }
+    ships {
+      name
+      url
+    }
+  }
+}
+`;
 
 export default function Mission() {
+  const history = useHistory();
+  const location = useLocation();
+  const search = location.search;
+  const match = search.match(/id=(.*)/);
+  const id = match?.[1];
+  if (!id) {
+    history.push({
+      pathname: '/'
+    });
+  }
+
+  const { loading, error, data } = useQuery(MISSION_QUERY, {
+    variables: { id },
+  });
+
+  if (error) return <div>{error.message}</div>;
+  const launch = data ? data.launch : null;
+  
   return (
-    <div>
-      Mission
-    </div>
+    <Grid container spacing={0} justifyContent='center' className='mission'>
+      {loading || !launch ?
+        <Grid item xs={8} className='loading-launches'>
+          <div>Loading...</div>
+        </Grid>
+        :
+        <Grid
+          item
+          xs={8}
+          key={launch.id}
+          className={'mission-box'}
+        >
+          <DetailBox {...launch} />
+        </Grid>
+      }
+    </Grid>
   );
 }
